e83aa6da36121dfd15e874f79911068eff35c8c0
aespinosar <aespinosar96@gmail.com>
2025-07-18 23:20:14 -0600

    Preparación de una nueva página

--- 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
+++ 02-bases/src/app/pages/dragonball-super/dragonball-super-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,27 +8,19 @@ interface Character {
 }
 
 @Component({
-  selector: 'app-dragonball-page',
+  selector: 'app-dragonball-super-page',
   imports: [],
-  templateUrl: './dragonball-page.component.html',
-  styleUrl: './dragonball-page.component.css'
+  templateUrl: './dragonball-super-page.component.html',
+  styleUrl: './dragonball-super-page.component.css'
 })
-export class DragonballPageComponent {
+export class DragonballSuperPageComponent {
   name = signal('');
   power = signal(0);
 
   characters = signal<Character[]>([
     {id: 1, name: 'Goku', power: 9001 },
-    // {id: 2, name: 'Vegeta', power: 8000 },
-    // {id: 4, name: 'Yamcha', power: 500 },
-    // {id: 3, name: 'Piccolo', power: 3000 },
+    {id: 2, name: 'Vegeta', power: 8000 }
   ]);
-
-  // powerClasses = computed(() => {
-  //   return {
-  //     'text-danger': true,
-  //   };
-  // });
 
   addCharacter = () => {
     if (!this.name() || !this.power() || this.power() < 0) {
@@ -40,8 +32,6 @@ export class DragonballPageComponent {
       name: this.name(),
       power: this.power()
     };
-
-    // this.characters().push(newCharacter);
 
     this.characters.update((list => [...list, newCharacter]))
 
